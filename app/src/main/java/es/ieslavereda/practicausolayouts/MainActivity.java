7a4441d0b7738fa29615d9d49ae1139f245de167
Ivan Perez <ivan_psh@hotmail.com>
2023-03-10 16:36:26 +0100

    Update

--- app/src/main/java/es/ieslavereda/practicausolayouts/MainActivity.java
+++ app/src/main/java/es/ieslavereda/practicausolayouts/MainActivity.java
@@ -3,12 +3,25 @@ package es.ieslavereda.practicausolayouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button b1;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button1);
+        textView = findViewById(R.id.textView5);
+        b1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                textView.setText(b1.getText());
+            }
+        });
     }
 }
